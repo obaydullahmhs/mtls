5e31d13b642517367d4819716e8c16d7537973e4
obaydullahmhs <obaydullah@appscode.com>
2023-05-04 11:20:20 +0600

    C3:main verification in server added

--- client/client.go
+++ client/client.go
@@ -15,14 +15,20 @@ func main() {
 	if err != nil {
 		log.Fatalf("could not open certificate file: %v", err)
 	}
-
 	caCertPool := x509.NewCertPool()
 	caCertPool.AppendCertsFromPEM(cert)
+
+	certificate, err := tls.LoadX509KeyPair("../certs/client.crt", "../certs/client.key")
+	if err != nil {
+		log.Fatalf("could not load certificate: %v", err)
+	}
+
 	client := http.Client{
 		Timeout: time.Minute * 3,
 		Transport: &http.Transport{
 			TLSClientConfig: &tls.Config{
-				RootCAs: caCertPool,
+				RootCAs:      caCertPool,
+				Certificates: []tls.Certificate{certificate},
 			},
 		},
 	}
